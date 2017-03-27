d85be862486b1bcea0a75f30d440196d918d4d84
Sigma development <Sigma development@20.20.1.192>
2017-03-27 07:22:03 -0300

    Teste GitHub

--- AlocaSIS/src/model/Funcionario.java
+++ AlocaSIS/src/model/Funcionario.java
@@ -1,3 +1,5 @@
+package model;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +11,23 @@
  * @author Sigma development
  */
 public class Funcionario {
+    private String nome;
+    private String rg;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+    
 }
