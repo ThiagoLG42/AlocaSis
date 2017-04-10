30b15c4fd7a37db6c9a18cae6740ed5a648aed7c
Sigma development <Sigma development@20.20.1.192>
2017-04-10 16:53:17 -0300

    Criação datela de login e da classe de conexão como banco de dados

--- AlocaSIS/src/view/cliente/TelaPrincipal.java
+++ AlocaSIS/src/view/cliente/TelaPrincipal.java
@@ -141,6 +141,9 @@ public class TelaPrincipal extends javax.swing.JFrame {
 
     private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
         // TODO add your handling code here:
+        AlterarCliente tela = new AlterarCliente();
+        telaPrincipal.add(tela);
+        tela.setVisible(true);
     }//GEN-LAST:event_jMenuItem4ActionPerformed
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
